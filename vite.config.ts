3f0b4ceca7f71d5a8e1ee0c02a28340f2d53d5b7
Nick K <nick.kruchinin@gmail.com>
2024-01-13 14:43:38 +0300

    Add a workaround using 3rd party plugin for ts compilation

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,33 @@
+import ts from "typescript"
+
 import {defineConfig, type UserConfig} from "vite"
 import {qwikVite} from "@builder.io/qwik/optimizer"
 import {qwikCity} from "@builder.io/qwik-city/vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
+import {
+  vitePluginTypescriptTransform
+} from "vite-plugin-typescript-transform"
+
 export default defineConfig((): UserConfig => ({
-  plugins: [qwikCity({trailingSlash: false}), qwikVite(), tsconfigPaths()],
+  plugins: [
+    qwikCity({trailingSlash: false}),
+    qwikVite(),
+    tsconfigPaths(),
+    vitePluginTypescriptTransform({
+      enforce: "pre",
+      filter: {
+        files: {
+          include: /.tsx?$/,
+        }
+      },
+      tsconfig: {
+        override: {
+          jsx: ts.JsxEmit.Preserve
+        }
+      }
+    }),
+  ],
   server: {
     headers: {
       "Cache-Control": "public, max-age=0"
